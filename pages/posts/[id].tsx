2babf326b07a2ea254f9717b5f19c19c159101c8
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2022-06-13 13:19:19 +0300

    [docs]: Static generation with your page path dependin on external data

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,40 @@
+function Post({ post }: any) {
+  return (
+    <>
+      <div>User: {post.userId}</div>
+      <article>Article#{post.id}: {post.title}</article>
+      <main>Content: {post.body}</main>
+    </>
+  );
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  const posts = await res.json();
+
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post: any) => ({
+    params: { id: post.id.toString() },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }: any) {
+  console.log(params);
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const post = await res.json();
+  // Pass post data to the page via props
+  return { props: { post } };
+}
+
+export default Post;
